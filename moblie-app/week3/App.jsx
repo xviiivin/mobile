d170290aad6a84fcd4339d24b7d7dc8586d9af81
wiwat liangkobkit <wiwatwin1801@gmail.com>
2023-07-27 00:43:51 +0700

    fix:ex3.1

--- moblie-app/week3/App.jsx
+++ moblie-app/week3/App.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View,ScrollView  } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import Home from "./screens/Home";
 export default function App() {
   return (
-    <ScrollView  style={styles.container}>
-      <Home />
-    </ScrollView >
+    <View style={styles.container}>
+      <ScrollView>
+        <Home />
+      </ScrollView>
+    </View>
   );
 }
 
---
+++ moblie-app/week3/screens/Home.jsx
@@ -0,0 +1,60 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
+import React, { useState } from "react";
+
+const Home = () => {
+  const [word, setWord] = useState("");
+  const [wordList, setWordList] = useState([]);
+
+  const handleAddWord = () => {
+    if (word.trim() !== "") {
+      setWordList((prevList) => [...prevList, { word: word.trim() }]);
+      setWord("");
+    }
+  };
+  const renderItem = ({ item }) => (
+    <Text style={{ fontSize: 18 }}>{item.word}</Text>
+  );
+  return (
+    <View>
+      <Text style={styles.header}>สมุดบันทึก</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="จะใส่อะไรก็ใส่"
+        value={word}
+        onChangeText={(text) => setWord(text)}
+      />
+      <Button
+        title="กดซะถ้าอยากเก็บข้อมูล"
+        color="#841584"
+        onPress={handleAddWord}
+      />
+      <FlatList data={wordList} renderItem={renderItem} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    paddingTop: 30,
+    fontSize: 30,
+  },
+  input: {
+    height: 60,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  mt: {
+    marginTop: 20,
+  },
+});
+
+export default Home;
