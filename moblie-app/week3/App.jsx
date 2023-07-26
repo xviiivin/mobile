12f4ceb23746d6882cfe9df5d1e30e7af286b3e2
wiwat liangkobkit <wiwatwin1801@gmail.com>
2023-07-27 00:05:38 +0700

    fix: ex3.1

--- moblie-app/week3/App.jsx
+++ moblie-app/week3/App.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View,ScrollView  } from "react-native";
 import Home from "./screens/Home";
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView  style={styles.container}>
       <Home />
-    </View>
+    </ScrollView >
   );
 }
 
