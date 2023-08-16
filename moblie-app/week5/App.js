aea97d1505127106595c5b78fd4e8fb6c244912b
wiwat liangkobkit <wiwatwin1801@gmail.com>
2023-08-16 23:20:08 +0700

    update: week5

---
+++ moblie-app/week5/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+// import คอมโพเนนต์ที่จำเป็น
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const MealsNavigator = createNativeStackNavigator();
+
+export default function App() {
+  // เพิ่มโค้ดส่วนนี้ เพื่อจัดการ Stack Navigation
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
