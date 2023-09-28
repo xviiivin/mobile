ef89fb94a70d145e388b19db3bd5ae55551405e8
wiwat liangkobkit <wiwatwin1801@gmail.com>
2023-09-28 13:14:23 +0700

    lab6

---
+++ moblie-app/Lab06/App.js
@@ -0,0 +1,13 @@
+import Lab04 from './Components/Lab04/Lab04';
+import Lab05 from './Components/Lab05/Lab05';
+import Lab06 from './Components/Lab06/Lab06';
+import Lab1_1 from './Components/lab01';
+import {Lab2_1, Lab2_2} from './Components/lab02';
+import {Lab3_1, Lab3_2} from './Components/lab03';
+
+
+export default function App() {
+  return (
+    <Lab06 />
+  );
+}
